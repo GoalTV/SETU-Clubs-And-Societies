1c1ec2301ed1f75840db47ef274f1593d29e197d
GoalTV <janusz@mailapp.red>
2023-03-10 17:50:34 +0000

    Added include bootstrap.bundle.js
    due issue with dropdown menu not loading.

--- template/header_tpl.php
+++ template/header_tpl.php
@@ -31,6 +31,7 @@
         <link rel="stylesheet" type="text/css" href="css/custom.css">
         <script src="js/jquery-3.6.4.min.js"></script>
         <script src="js/bootstrap.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
 
         <!-- Fontawesome Icons -->
         <script src="https://kit.fontawesome.com/637e7e9f05.js" crossorigin="anonymous"></script>
